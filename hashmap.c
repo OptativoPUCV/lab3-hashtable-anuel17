b91d8ee1de663aeefc37749ebce9aad3126d98ff
Replit user <>
2023-09-05 02:51:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,14 +41,12 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value){
   for(long i = 0; i < map->capacity; i++){
-    long index = (hash(key, map->capacity) + i) % map->capacity;
+    long index= (hash(key, map->capacity) + i)%map->capacity;
     struct Pair *currentPair = map->buckets[index];
-    if (currentPair == NULL || currentPair->key == NULL) {
+    if(currentPair == NULL || currentPair->key == NULL){
       map->buckets[index] = createPair(key, value);
       map->current = index;
       map->size++;
-      return;
-    }else if (strcmp(currentPair->key, key) == 0){
       return;
     }
   }
