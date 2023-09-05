b2939e934fda8fab085cdb4bb5d6172feee1af99
Replit user <>
2023-09-05 22:05:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -114,8 +114,14 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  for(i = 0; i < map->capacity;i+){
+    struct Pair* currentpair = map->buckets[i];
+    if(currentpair == NULL || currentpair->key != NULL){
+      map->current = i;
+      return currentpair;
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
